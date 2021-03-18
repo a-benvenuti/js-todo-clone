6dc8854b1309b671337d5f80415e62809b8ceef2
Andrea <b.a.benvenuti3@gmail.com>
2021-03-18 15:12:10 +0100

    Config. iniziale

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var elenco = [
+  'comprare pane',
+  'pagare bolleta luce',
+  'chiamare luigi',
+];
+
+// for (var i = 0; i < elenco.length; i++) {
+//   // clono il contenuto del template
+//   var template = $('.template li').clone();
+//   // aggiungo le info al template
+//   template.prepend(elenco[i]);
+//   // aggiungo il template alla lista
+//   $('.to_do_list')append(template);
+// }
+//
+// $("#aggiungi").keydown(function(evento){
+//   if(evento.which == 13){
+//     var testo = $(this).val();
+//     // clono il contenuto del template
+//     var template = $('.template li').clone();
+//     // aggiungo le info al template
+//     template.prepend(testo);
+//     // aggiungo il template alla lista
+//     $('.to_do_list')append(template);
+//     // riazzero il contenuto di "aggiungi"
+//     $(this).val("");
+//   }
+// });
+
+$(".to_do_list").on('click', '.to_do_list-delate', function(){
+  $(this).parent().remove();
+});
