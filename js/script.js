ad3980bda306513b49bebfb5ef4befd7b1e1b180
Andrea <b.a.benvenuti3@gmail.com>
2021-03-18 15:33:18 +0100

    corretto il problema

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ for (var i = 0; i < elenco.length; i++) {
   // aggiungo le info al template
   template.prepend(elenco[i]);
   // aggiungo il template alla lista
-  $('.to_do_list')append(template);
+  $('.to_do_list').append(template);
 }
 
 $("#aggiungi").keydown(function(evento){
@@ -22,7 +22,7 @@ $("#aggiungi").keydown(function(evento){
       // aggiungo le info al template
       template.prepend(testo);
       // aggiungo il template alla lista
-      $('.to_do_list')append(template);
+      $('.to_do_list').append(template);
       // riazzero il contenuto di "aggiungi"
       $(this).val("");
     }
