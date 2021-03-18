acbb16b84d4814b694a923c6b45a945c40f4cd07
Andrea <b.a.benvenuti3@gmail.com>
2021-03-18 16:37:51 +0100

    creata funzione 'clonazione'

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
-var elenco = [
-  'comprare pane',
-  'pagare bolleta luce',
-  'chiamare luigi',
-];
-
-for (var i = 0; i < elenco.length; i++) {
+// -----------------------------------------------FUNZIONI----------------------------------------------------
+/*
+  CLONAZIONE DI UNO O PIU ELEMENTI, FIGLI DI UN  ELEM.'CONTAINER' NON VISIBILE;
+  E 'TRAPIANTATI' CON DELLE NUOVE 'INFO' IN UN ALTRA SEZIONE DI CODICE A SCELTA NOSTRA
+  esempi:
   // clono il contenuto del template
   var template = $('.template li').clone();
   // aggiungo le info al template
   template.prepend(elenco[i]);
   // aggiungo il template alla lista
   $('.to_do_list').append(template);
+
+  IMPORTANTE!
+  prima di chiamare la funzione 'clonazione' creare la varbiabile denominata 'variabileContenitoreClonato'
+*/
+function clonazione(variabileContenitoreClonato, elemCheVoglioClonare, newInfoClonato, doveDeveEssereClonato){
+  // clono qualsiasi cosa e la metto in una variabile
+  var variabileContenitoreClonato = $(elemCheVoglioClonare).clone();
+  // aggiungo le info all'elemento che ho clonato
+  variabileContenitoreClonato.prepend(newInfoClonato);
+  // aggiungo l'elemento clonato con le nuove info dove voglio
+  $(doveDeveEssereClonato).append(variabileContenitoreClonato);
+}
+// ------------------------------------------fine delle FUNZIONI----------------------------------------------
+var elenco = [
+  'comprare pane',
+  'pagare bolleta luce',
+  'chiamare luigi',
+];
+var template;
+
+for (var i = 0; i < elenco.length; i++) {
+  clonazione(template, '.template li', elenco[i], '.to_do_list');
 }
 
 $("#aggiungi").keydown(function(evento){
   if(evento.which == 13){
     var testo = $(this).val();
     if (testo != "") {
-      // clono il contenuto del template
-      var template = $('.template li').clone();
-      // aggiungo le info al template
-      template.prepend(testo);
-      // aggiungo il template alla lista
-      $('.to_do_list').append(template);
+      clonazione(template, '.template li', testo, '.to_do_list')
       // riazzero il contenuto di "aggiungi"
       $(this).val("");
     }
